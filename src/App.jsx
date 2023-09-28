204c72d4c60970540ee79f3e3b57db5131c90e54
Efrain <relyekmind@gmail.com>
2023-09-27 22:04:29 -0500

    Primer comit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <ItemListContainer />
+    </div>
+  );
+}
+
+export default App;
