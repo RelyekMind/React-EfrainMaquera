48feee43d2661e2feb4bae634780f9dbb6cc3b7f
Efrain <relyekmind@gmail.com>
2023-10-16 19:07:51 -0500

    segunda pre entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import ItemListContainer from "./components/ItemList";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-      <ItemListContainer />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/productos" element={<ItemListContainer />} />
+          <Route path="/productos/:categoria" element={<ItemListContainer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
